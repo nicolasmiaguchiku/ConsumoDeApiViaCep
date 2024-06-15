c9ee9583391729c3c793c018703492b5f60297de
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2024-06-15 13:34:08 -0300

    Consumo de API ViaCep

---
+++ Entities/Services/EnderecoServices.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using ConsumindoApiViaCep.Entities;
+using System.Threading.Tasks;
+
+namespace ConsumindoApiViaCep.Entities.Services
+{
+    internal class EnderecoServices
+    {
+        public async Task<Endereco> Integracao(string cep) 
+        {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync($"https://viacep.com.br/ws/{cep}/json/");
+            var jsonString = await response.Content.ReadAsStringAsync();
+
+            var jsonObject = JsonConvert.DeserializeObject<Endereco>(jsonString);
+
+            if(jsonObject is not null)
+            {
+                return jsonObject;
+            }
+            else
+            {
+                return new Endereco
+                {
+                    Verificacao = true
+                };
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsumindoApiViaCep.Entities;
+using ConsumindoApiViaCep.Entities.Services;
+using System;
+
+namespace ConsumindoApidViaCep
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.Write("Informe o cep: ");
+            string? cep = Console.ReadLine();
+
+
+            EnderecoServices? enderecoServices = new EnderecoServices();
+
+            Endereco enderecoEncontrado = await enderecoServices.Integracao(cep);
+
+            if (!enderecoEncontrado.Verificacao)
+            {
+                Console.WriteLine("Endereco encontrado: ");
+                Console.WriteLine("Cep: "+ enderecoEncontrado.Cep);
+                Console.WriteLine("Logradouro: " + enderecoEncontrado.Logradouro);
+                Console.WriteLine("Complemento: " + enderecoEncontrado.Complemento);
+                Console.WriteLine("Bairro: " + enderecoEncontrado.Bairro);
+                Console.WriteLine("Localidade: " + enderecoEncontrado.Localidade);
+                Console.WriteLine("Uf: " + enderecoEncontrado.Uf);
+            }
+            else
+            {
+               Console.WriteLine("Endereco não encontrado");
+            }
+        }
+    }
+}
